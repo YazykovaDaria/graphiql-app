f880e23d4b25eed988b845609b1d0a77b6421162
YazykovaDaria <yazykovadasva@gmail.com>
2023-04-26 22:25:07 +0300

    feat: add workfloy

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { configDefaults } from 'vitest/dist/config';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   test: {
     globals: true,
     environment: 'jsdom',
