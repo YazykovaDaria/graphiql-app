9854a626f973d297c2b2e6fc646b3b517e6a13a5
YazykovaDaria <yazykovadasva@gmail.com>
2023-05-04 23:06:53 +0300

    refactor: lint

--- i18n-parser.config.cjs
+++ i18n-parser.config.cjs
@@ -2,10 +2,12 @@ module.exports = {
   defaultNamespace: 'translation',
   locales: ['en', 'ru'],
   output: 'public/locales/$LOCALE/$NAMESPACE.json',
-  jsx: [{
-    lexer: 'JsxLexer',
-    attr: 'i18nKey',
-    componentFunctions: ['Trans'],
-  }],
-  input: [ 'src/**/*.{ts,tsx}', ],
-}
+  jsx: [
+    {
+      lexer: 'JsxLexer',
+      attr: 'i18nKey',
+      componentFunctions: ['Trans'],
+    },
+  ],
+  input: ['src/**/*.{ts,tsx}'],
+};
