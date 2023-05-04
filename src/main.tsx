28bdeec227613fa94e582e0ed24553b70baab350
YazykovaDaria <yazykovadasva@gmail.com>
2023-05-04 22:47:39 +0300

    refactor: reviews remarks

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
 import './index.css';
 import './i18next/i18n';
+import { Preloader } from './components/langSwitcher/preloader/Preloader';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <Suspense fallback='...loading'>
+    <Suspense fallback={<Preloader />}>
       <App />
     </Suspense>
   </StrictMode>
