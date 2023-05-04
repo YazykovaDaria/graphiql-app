f674bb7f9c1afba69c915b9dbb1a22e7637bdf91
YazykovaDaria <yazykovadasva@gmail.com>
2023-05-04 16:10:16 +0300

    feat: switch lang

---
+++ src/components/LangSwitcher.tsx
@@ -0,0 +1,55 @@
+import flagEn from 'src/assets/img/united-kingdom.png';
+import flagRu from 'src/assets/img/russia.png';
+import { useTranslation } from 'react-i18next';
+import { Select, MenuItem, Avatar, SelectChangeEvent } from '@mui/material';
+
+type Locale = {
+  img: string;
+};
+
+type Locales = Record<string, Locale>;
+
+const locales: Locales = {
+  en: { img: flagEn },
+  ru: { img: flagRu },
+};
+
+function LangSwitcher() {
+  const { i18n } = useTranslation();
+  const defaultLang = i18n.resolvedLanguage;
+
+  const switchLang = (e: SelectChangeEvent) => {
+    const lang = e.target.value;
+    i18n.changeLanguage(lang);
+  };
+
+  return (
+    <Select
+      value={defaultLang}
+      onChange={switchLang}
+      size='small'
+      sx={{
+        '& .MuiSelect-select': {
+          py: 0,
+          px: 1,
+        },
+      }}
+    >
+      {Object.keys(locales).map((locale) => (
+        <MenuItem value={locale} key={locale}>
+          <Avatar
+            variant='rounded'
+            alt={`flag ${locale}`}
+            src={locales[locale].img}
+            style={{
+              width: 50,
+              height: 50,
+            }}
+          />
+        </MenuItem>
+      ))}
+    </Select>
+  );
+}
+
+export default LangSwitcher;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import { StrictMode, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
 import './index.css';
+import './i18next/i18n';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <Suspense fallback='...loading'>
+      <App />
+    </Suspense>
+  </StrictMode>
 );
