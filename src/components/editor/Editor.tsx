75e6365dc1d7ba727633944d571f687ef94ffa89
YazykovaDaria <yazykovadasva@gmail.com>
2023-05-16 13:15:53 +0300

    feat: logic for query field

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,25 +1,30 @@
-import { FormEvent } from 'react';
+import { FocusEvent } from 'react';
 import { useAppSelector, useAppDispatch } from 'src/hooks/reduxHooks';
 import { updateQuery } from 'src/store/slices/editorSlice';
 import { TextField } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
 export function Editor() {
+  const { t } = useTranslation();
   const { query } = useAppSelector((state) => state.editor);
   const dispatch = useAppDispatch();
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const inputValue = (e.currentTarget[0] as HTMLTextAreaElement).value;
-    // console.log(inputValue);
+  const handleBlur = (e: FocusEvent<HTMLTextAreaElement>) => {
+    const inputValue = e.target.value;
     dispatch(updateQuery(inputValue));
-    console.log(query);
   };
 
-  // console.log(query);
-
+  console.log(query);
   return (
-    <form onSubmit={handleSubmit}>
-      <TextField sx={{ width: '100%', height: '100%', border: 'none' }} />
+    <form>
+      <TextField
+        sx={{ width: '100%', height: '100%', '& fieldset': { border: 'none' } }}
+        onBlur={handleBlur}
+        placeholder={t('main.editorPlaceholder') as string}
+        multiline
+        fullWidth
+        maxRows={100}
+      />
     </form>
   );
 }
