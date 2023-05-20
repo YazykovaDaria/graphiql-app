9db891a819b7980cc5be2756e9d413f76f72a186
YazykovaDaria <yazykovadasva@gmail.com>
2023-05-20 11:20:06 +0300

    feat: redux setup for test

---
+++ src/tests/utils/reduxForTest.tsx
@@ -0,0 +1,34 @@
+import { PreloadedState } from '@reduxjs/toolkit';
+import { render, RenderOptions } from '@testing-library/react';
+import { ReactElement, PropsWithChildren } from 'react';
+import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
+import i18n from 'src/i18next/i18nForTests';
+
+import { setupStore, AppStore, RootState } from 'src/store/store';
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>;
+  store?: AppStore;
+}
+
+function renderWithProviders(
+  ui: ReactElement,
+  {
+    preloadedState = {},
+    store = setupStore(preloadedState),
+    ...renderOptions
+  }: ExtendedRenderOptions = {}
+) {
+  function Wrapper({ children }: PropsWithChildren<object>): JSX.Element {
+    return (
+      <Provider store={store}>
+        <I18nextProvider i18n={i18n}> {children}</I18nextProvider>
+      </Provider>
+    );
+  }
+
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
+
+export default renderWithProviders;
