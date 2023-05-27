831339cd293a75738d806668eae0b8936969c8c5
YazykovaDaria <yazykovadasva@gmail.com>
2023-05-28 00:19:34 +0300

    feat: init func

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,9 +1,34 @@
+import { useGetGraphQueryMutation } from 'src/store/api/graphQueryApi';
+import { useEffect } from 'react';
+import { useAppDispatch } from 'src/hooks/reduxHooks';
+import { addScheema } from 'src/store/slices/docsSlice';
+
 import { Toolbar } from 'src/components/toolbar/Toolbar';
 import { Editor } from 'src/components/editor/Editor';
 import { ResponseSection } from 'src/components/responseSection/ResponseSection';
 import { Grid } from '@mui/material';
+import { DocsExplorer } from 'src/components/docsExplorer/DocsExplorer';
+import { queryForScheema } from './const';
 
 export function MainPage() {
+  const dispatch = useAppDispatch();
+  const [getDocs] = useGetGraphQueryMutation();
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const res = await getDocs({ newQuery: queryForScheema });
+        if ('data' in res) {
+          dispatch(addScheema(res.data));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    init();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Grid
       container
@@ -38,6 +63,8 @@ export function MainPage() {
       <Grid item xs={12} sm={6} component='section'>
         <ResponseSection />
       </Grid>
+
+      <DocsExplorer />
     </Grid>
   );
 }
