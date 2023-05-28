3ab9deb7653c2f4356238a8278980b796f8e6382
ivan52945 <ivan52945@gmail.com>
2023-05-28 17:24:27 +0300

    fead: add raw adaptation

--- src/components/docsExplorer/DocsExplorer.tsx
+++ src/components/docsExplorer/DocsExplorer.tsx
@@ -1,10 +1,14 @@
 import { useAppSelector } from 'src/hooks/reduxHooks';
-import { Container } from '@mui/material';
+import { Container, useMediaQuery } from '@mui/material';
 import { DocsRouter } from './DocsRouter';
 
 export function DocsExplorer() {
   const { scheema, isOpen } = useAppSelector((state) => state.docs);
   const { data } = scheema;
+
+  const query = useMediaQuery('(min-width:600px)');
+
+  const translateDir = query ? 'X' : 'Y';
 
   return (
     <Container
@@ -13,11 +17,11 @@ export function DocsExplorer() {
         top: '0',
         right: '0%',
         transition: '0.4s',
-        transform: isOpen ? 'translateX(200%)' : 'translateX(0)',
+        transform: isOpen ? `translate${translateDir}(200%)` : `translate${translateDir}(5px)`,
         height: '100%',
         backgroundColor: 'white',
         zIndex: 2,
-        background: '#FCFCFC',
+        background: '#ECFFE2',
         borderRadius: '10px',
       }}
     >
