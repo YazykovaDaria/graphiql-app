551e512728fbe7eec8ef00602ec6f14369f8b60a
Doooodie <unfair.slenderman@gmail.com>
2023-05-28 14:15:06 +0200

    feat: change 404 page design

--- src/pages/notFound/NotFoundPage.tsx
+++ src/pages/notFound/NotFoundPage.tsx
@@ -1,7 +1,18 @@
+import { Container, Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+
 export function NotFoundPage() {
+  const { t } = useTranslation();
+
   return (
-    <div>
-      <h2>Page not found</h2>
-    </div>
+    <Container
+      component='main'
+      maxWidth='xl'
+      sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mt: 'auto' }}
+    >
+      <Typography component='h1' variant='h5' gutterBottom>
+        {t('layout.no-page-error')} 😔
+      </Typography>
+    </Container>
   );
 }
