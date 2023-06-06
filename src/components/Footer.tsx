b235983add012edd29ecf002cbf374c99c083214
YazykovaDaria <yazykovadasva@gmail.com>
2023-06-06 10:32:00 +0300

    refactor: structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -43,6 +43,15 @@ export function Footer() {
           <Grid item>
             <Item>
               <Link
+                href='https://github.com/YazykovaDaria'
+                target='_blank'
+                color='inherit'
+                underline='hover'
+              >
+                Daria
+              </Link>
+              <Typography component='span'>, </Typography>
+              <Link
                 href='https://github.com/Doooodie'
                 target='_blank'
                 color='inherit'
@@ -50,7 +59,8 @@ export function Footer() {
               >
                 Doodie
               </Link>
-              <Typography component='span'>, </Typography>
+
+              <Typography component='span'> & </Typography>
               <Link
                 href='https://github.com/ivan52945'
                 target='_blank'
@@ -58,15 +68,6 @@ export function Footer() {
                 underline='hover'
               >
                 Nemicus
-              </Link>
-              <Typography component='span'> & </Typography>
-              <Link
-                href='https://github.com/YazykovaDaria'
-                target='_blank'
-                color='inherit'
-                underline='hover'
-              >
-                Daria
               </Link>
               <Typography component='span'>, 2023</Typography>
             </Item>
--- src/pages/welcome/WelcomePage.tsx
+++ src/pages/welcome/WelcomePage.tsx
@@ -1,50 +1,5 @@
-import { useTranslation } from 'react-i18next';
-import { Typography, Grid, Container, Stack } from '@mui/material';
-import { WelcomePapers } from 'src/components/WelcomePapers';
-import { WelcomeHeading } from 'src/components/WelcomeHeading';
-import { WelcomeScroll } from 'src/components/WelcomeScroll';
-import { WelcomeAvatars } from 'src/components/WelcomeAvatars';
+import { WelocmeSection } from 'src/components/welcomeSection/WelcomeSection';
 
 export function WelcomePage() {
-  const { t } = useTranslation();
-
-  return (
-    <Container component='main' maxWidth='xl' sx={{ my: 10 }}>
-      <Grid
-        container
-        alignItems='center'
-        justifyContent='center'
-        rowSpacing={{ xs: 4, sm: 8, md: 12 }}
-        columnSpacing={{ xs: 4, sm: 8, md: 12 }}
-      >
-        <WelcomeHeading />
-        <WelcomeScroll />
-
-        <Grid item component='section' xs={12} md={7} order={4} textAlign='center'>
-          <Stack spacing={4}>
-            <Typography variant='h3'>{t('hello.info.title')}</Typography>
-            <Typography variant='subtitle1' color='text.secondary'>
-              {t('hello.info.content')}
-            </Typography>
-          </Stack>
-        </Grid>
-
-        <WelcomePapers />
-
-        <Grid item component='section' xs={12} md={7} order={11} textAlign='center'>
-          <Stack spacing={4}>
-            <Typography variant='h3'>RS School</Typography>
-            <Typography variant='subtitle1' color='text.secondary'>
-              {t('hello.course.about')}
-            </Typography>
-            <Typography variant='subtitle1' color='text.secondary'>
-              {t('hello.course.info')}
-            </Typography>
-          </Stack>
-        </Grid>
-
-        <WelcomeAvatars />
-      </Grid>
-    </Container>
-  );
+  return <WelocmeSection />;
 }
